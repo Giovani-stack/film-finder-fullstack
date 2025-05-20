7c24d83d89c7afd6f19fcf84a74eda2d4a1d2d8c
Gigi <gigi@refresh-academy.org>
2025-05-20 21:15:55 +0200

    API fetches from themoviedb when /discover/movie doesn't find a local copy of genre movies

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require('express');
 const fs = require('node:fs');
 const bodyParser = require('body-parser');
 
+const tmdbBaseUrl = 'https://api.themoviedb.org/3';
+const tmdbKey = '4048775a0f068af3048837ff0341a4f7';
+
 const app = express()
 const port = 3000
 app.use(express.static("public"));
@@ -16,10 +19,31 @@ app.get('/genre/movie/list', (req, res) => {
   res.send(genres)
 });
 
-app.get('/discover/movie', (req, res) => {
+app.get('/discover/movie', async (req, res) => {
   console.log("/discover/movie params: ", req.query)
   const genreId = req.query.with_genres; // <-- SECURITY THREAT!
-  const dataAsText = fs.readFileSync(`data/genre-movies-${genreId}.json`, 'utf8');
+  const filePath = `data/genre-movies-${genreId}.json`;
+  if (fs.existsSync(filePath)) {
+    console.log(`Found movies for genre ${genreId} in local DB! YAY!`);
+  } else {
+    console.log(`Can not find movies for genre ${genreId}: fetch from themoviedb!`)
+    const discoverMovieEndpoint = '/discover/movie';
+    const requestParams = `?api_key=${tmdbKey}&with_genres=${genreId}`;
+    // const urlToFetch = tmdbBaseUrl+discoverMovieEndpoint+requestParams;
+    const urlToFetch = tmdbBaseUrl + discoverMovieEndpoint + requestParams;
+    try {
+      const response = await fetch(urlToFetch);
+      if (response.ok) {
+        const jsonResponse = await response.json();
+        const movies = jsonResponse.results;
+        console.log(`Got ${movies.length} movies for genre ${genreId}`);
+        fs.writeFileSync(filePath, JSON.stringify(jsonResponse))
+      }
+    } catch (e) {
+      console.log(" Error while getting movies: ", e);
+    }
+  }
+  const dataAsText = fs.readFileSync(filePath, 'utf8');
   const genreMovies = JSON.parse(dataAsText);
   res.send(genreMovies);
 })
@@ -28,7 +52,7 @@ app.post('/api/movie/like', (req, res) => {
  console.log("Request body: ", req.body.movieId);
  const responseObj = { message: 'Data received successfully', yourData: req.body };
  try {
-  const votesText = fs.readFileSync(`data/votes.json`, 'utf8'); 
+  const votesText = fs.readFileSync(`data/votes.json`, 'utf8');
   const votesObj = JSON.parse(votesText);
   console.log("votesObj BEFORE PUSH", votesObj);
   votesObj.likes.push(req.body.movieId);
